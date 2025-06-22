cbe03297509bf86c19125218661c7529f1dcb2ad
matéo! <6matbub@gmail.com>
2025-06-21 22:27:07 -0700

    options update and gitsign keymaps

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,16 @@
 -- for the neovim aspect - https://neovim.io/doc/user/lua-guide.html#_vim.opt
 -- for list of options <:options>
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
 vim.opt.relativenumber=true
 vim.opt.number=true
 
 vim.g.have_nerd_font = true
+
+-- enable mouse mode
+vim.o.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -12,3 +19,36 @@ vim.g.have_nerd_font = true
 vim.schedule(function()
   vim.o.clipboard = 'unnamedplus'
 end)
+
+-- https://stackoverflow.com/questions/1204149/smart-wrap-in-vim
+vim.o.breakindent = true
+
+-- save undo history
+vim.o.undofile = true
+
+-- case insensitive searching unless 
+-- \C or one or more chararacters have cap in search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- decrease update time 
+vim.o.updatetime = 250
+
+-- decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- configure whitespace in editor 
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- https://www.youtube.com/watch?v=sA3z6gsqOuw
+vim.o.inccommand = 'split'
+
+-- show the line your on
+vim.o.cursorline = true
+
+-- minimal num of screen lines to keep above and below the curosr
+vim.o.scrolloff = 10 
+
+-- clear highlights on search
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
