85ed4686a1351cef298d8b6bc48ea35eaaa0333f
matéo! <6matbub@gmail.com>
2025-08-02 00:30:43 -0700

    modify alpine linux script to include docker setup

--- alpine_linux_setup.sh
+++ alpine_linux_setup.sh
@@ -1,6 +1,21 @@
-sudo apk update
+apk update
+apk upgrade
 
-# install clangd (NEOVIM DEPENDENCY)
-sudo apk add clang ripgrep luarocks unzip gzip
+# install docker
+apk add docker docker-cli
+
+# start docker daemon
+sudo service docker start
+
+# enable docker to start on boot
+# create a symbolic link from the Docker service script 
+# in /etc/init.d/docker to the boot runlevel directory.- 
+#	
+#	/etc/runlevels/boot/docker -> /etc/init.d/docker
+sudo rc-update add docker boot
+# theres no more docker shenanigans after this line
+
+# install my neovim config dependecies
+apk add clang ripgrep luarocks unzip gzip
 # install neovim
-sudo apk add neovim
+apk add neovim
